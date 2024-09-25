12a24f31b9647142ae5e62e758aa61143752394f
libanhsxbaoil <supa35vnua@gmail.com>
2024-09-25 13:23:29 +0700

    Create Test.js

---
+++ Module/Test.js
@@ -0,0 +1,10 @@
+#!name=AllinOne
+#!desc=UnlockPremium
+
+[Script]
+#Unlock
+#hostname = *.bendingspoonsapps.com
+http-response ^https?:\/\/.*\.bendingspoonsapps\.com\/v\d\/(users\/.+|purchases\/verify) requires-body=1,script-path=https://raw.githubusercontent.com/libanhsxbaoil/zozocao/refs/heads/main/Scripts/Test.js, tag=Test
+
+[MITM]
+hostname=*.bendingspoonsapps.com
